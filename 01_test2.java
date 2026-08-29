48809e719baff9e4ec0ebb60dd13dc25b0a7bb99
manishtakuli2000-design <manishtakuli2000@gmail.com>
2026-08-29 10:24:32 +0530

    added first file

---
+++ 01_test2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner ;
+ class userinput {
+public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+System.out.print("enter two numbers");
+
+int a = sc.nextInt();
+int b = sc.nextInt();
+int c =a+b;
+  System.out.print("sum is =" +c );
+}       
+}
+ 
